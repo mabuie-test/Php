c9df47fd2935239bbbfd738812c55469256f6a6d
JorgeMabuie <jorgemaabuie@gmail.com>
2025-07-23 02:37:55 +0200

    Update db.php

--- Bewithme/db.php
+++ Bewithme/db.php
@@ -1,21 +1,21 @@
 <?php
 session_start();
 
-$host   = 'localhost';
-$dbname = 'phil_asean';      // nome da BD
-$user   = 'seu_usuario_mysql';
-$pass   = 'sua_password_mysql';
-$charset= 'utf8mb4';
+$host    = 'localhost';
+$dbname  = 'philaded_Philaseanproviderwebsite';
+$user    = 'philaded_Philaseanproviderwebsite';
+$pass    = 'Philaseanproviderwebsite';
+$charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$dbname;charset=$charset";
 $options = [
-  PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-  PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-  PDO::ATTR_EMULATE_PREPARES   => false,
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
 ];
-try {
-  $pdo = new PDO($dsn, $user, $pass, $options);
-} catch (PDOException $e) {
-  exit("Erro na conexão: " . $e->getMessage());
-}
 
+try {
+    $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (PDOException $e) {
+    exit("Erro na conexão: " . $e->getMessage());
+}
